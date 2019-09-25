75e6fb6ab7bceeab2dffe4d14cd3388a50af7db6
shenwang <15116303251@163.com>
2019-09-25 23:09:28 +0800

    ribbon 负载均衡算法的重写

---
+++ springcloud-customer/src/main/java/com/king/rule/MySelfRule.java
@@ -0,0 +1,16 @@
+package com.king.rule;
+
+import com.netflix.loadbalancer.IRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySelfRule {
+
+
+    @Bean
+    public IRule myRule(){
+       // return new RandomRule();
+        return new RandomRule_ZY();
+    }
+}
--- springcloud-customer/src/main/java/com/king/springcloud/DeptConsumerApplication.java
+++ springcloud-customer/src/main/java/com/king/springcloud/DeptConsumerApplication.java
@@ -1,11 +1,14 @@
 package com.king.springcloud;
 
+import com.king.rule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="SPRINGCLOUD-PROVIDER",configuration = MySelfRule.class)
 public class DeptConsumerApplication {
 
     public static void main(String[] args) {
